1ed4733a4914c99558305929702f4d2f46de46d5
ElMathidones <seuemail@exemplo.commath.mendez.007@gmail.com>
2026-03-07 14:28:56 -0300

    Exercício TS

---
+++ script.ts
@@ -0,0 +1,10 @@
+function multiplicar(numero1: number, numero2: number): number {
+    return numero1 * numero2;
+}
+
+function saudar(nome: string): string {
+    return "Olá, " + nome;
+}
+
+console.log(multiplicar(4, 5));
+console.log(saudar("Mathias"));
